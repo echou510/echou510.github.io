9fab0c46c68c121f2b5cef9f93e1d91482d29aa8
Eric Chou <echou510@gmail.com>
2022-04-19 11:39:40 -0700

    Add files via upload

---
+++ Big_JavaA/code/slider/SliderFrameViewer.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class SliderFrameViewer
+{  
+   public static void main(String[] args)
+   {  
+      SliderFrame frame = new SliderFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
+}
+
